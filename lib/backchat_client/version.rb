50f1daa971123759b862d9b8d72620da8c495d20
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-02-07 17:01:05 +0100

    bumps version

--- lib/backchat_client/version.rb
+++ lib/backchat_client/version.rb
@@ -1,3 +1,3 @@
 module BackchatClient
-  VERSION = "0.2"
+  VERSION = "0.3"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,4 @@
 Backchat::Client.logger = Logger.new("/dev/null")
 API_KEY = "valid-api-key"
 INVALID_API_KEY = "invalid-api-key"
+BASE_URL="http://localhost:8080/1"
